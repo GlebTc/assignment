4701d96059b969e96f5cbdafc09bebc4a5b6eeb9
GlebTc <bob88tikhonov@gmail.com>
2024-10-24 11:17:54 -0400

    Trying POST instead of PUT

--- app/api/targets/route.ts
+++ app/api/targets/route.ts
@@ -10,15 +10,13 @@ export async function GET() {
   return NextResponse.json(targetsData);
 }
 
-export async function PUT(req: Request) {
+export async function POST(req: Request) {
   const newTargetData = await req.json();
 
   try {
-    // Write the new data to the targets.json file
     fs.writeFileSync(filePath, JSON.stringify(newTargetData, null, 2));
     return NextResponse.json({ message: 'Data updated successfully' }, { status: 200 });
   } catch (error) {
-    console.error('Error updating the file:', error);  // Log the error for debugging purposes
     return NextResponse.json({ error: 'Error updating the file' }, { status: 500 });
   }
 }
--- components/TargetTable.tsx
+++ components/TargetTable.tsx
@@ -11,7 +11,11 @@ const TargetTable: React.FC<TargetTableProps> = ({ chartData, isDarkMode }) => {
   const [editableData, setEditableData] = useState<Target[]>(chartData);
 
   // Handle input changes for the editable data
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, id: number, field: keyof Target) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    id: number,
+    field: keyof Target
+  ) => {
     const newData = editableData.map((target) =>
       target.id === id ? { ...target, [field]: e.target.value } : target
     );
@@ -20,26 +24,16 @@ const TargetTable: React.FC<TargetTableProps> = ({ chartData, isDarkMode }) => {
 
   // Function to save the modified data
   const handleSave = async (id: number) => {
-    const targetToUpdateIndex = editableData.findIndex((target) => target.id === id);
+    const targetToUpdate = editableData.find((target) => target.id === id);
 
-    if (targetToUpdateIndex !== -1) {
-      const updatedTarget = {
-        ...editableData[targetToUpdateIndex],
-        lastUpdated: new Date().toISOString(), // Update the "lastUpdated" field to the current date
-      };
-
-      const updatedData = [...editableData];
-      updatedData[targetToUpdateIndex] = updatedTarget;
-
-      setEditableData(updatedData);
-
+    if (targetToUpdate) {
       try {
         const res = await fetch('/api/targets', {
-          method: 'PUT',
+          method: 'POST', // Changed to POST
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify(updatedData),
+          body: JSON.stringify(editableData),
         });
 
         if (res.ok) {
@@ -57,89 +51,136 @@ const TargetTable: React.FC<TargetTableProps> = ({ chartData, isDarkMode }) => {
   return (
     <div
       className={`overflow-x-auto p-4 shadow-md rounded-lg ${
-        isDarkMode ? 'bg-gray-900 border border-gray-700 shadow-xl' : 'bg-white border border-gray-300 shadow-lg'
+        isDarkMode
+          ? 'bg-gray-900 border border-gray-700 shadow-xl'
+          : 'bg-white border border-gray-300 shadow-lg'
       } transition-shadow duration-300`}
     >
-      <table className="min-w-full border-collapse">
+      <table className='min-w-full border-collapse'>
         <thead>
           <tr>
-            <th className={`py-2 px-4 border-b ${isDarkMode ? 'border-gray-700 text-gray-100' : 'border-gray-300 text-gray-900'}`}>
+            <th
+              className={`py-2 px-4 border-b ${
+                isDarkMode
+                  ? 'border-gray-700 text-gray-100'
+                  : 'border-gray-300 text-gray-900'
+              }`}
+            >
               Name
             </th>
-            <th className={`py-2 px-4 border-b ${isDarkMode ? 'border-gray-700 text-gray-100' : 'border-gray-300 text-gray-900'}`}>
+            <th
+              className={`py-2 px-4 border-b ${
+                isDarkMode
+                  ? 'border-gray-700 text-gray-100'
+                  : 'border-gray-300 text-gray-900'
+              }`}
+            >
               Description
             </th>
-            <th className={`py-2 px-4 border-b ${isDarkMode ? 'border-gray-700 text-gray-100' : 'border-gray-300 text-gray-900'}`}>
+            <th
+              className={`py-2 px-4 border-b ${
+                isDarkMode
+                  ? 'border-gray-700 text-gray-100'
+                  : 'border-gray-300 text-gray-900'
+              }`}
+            >
               Markets
             </th>
-            <th className={`py-2 px-4 border-b ${isDarkMode ? 'border-gray-700 text-gray-100' : 'border-gray-300 text-gray-900'}`}>
+            <th
+              className={`py-2 px-4 border-b ${
+                isDarkMode
+                  ? 'border-gray-700 text-gray-100'
+                  : 'border-gray-300 text-gray-900'
+              }`}
+            >
               Last Updated
             </th>
-            <th className={`py-2 px-4 border-b ${isDarkMode ? 'border-gray-700 text-gray-100' : 'border-gray-300 text-gray-900'}`}>
+            <th
+              className={`py-2 px-4 border-b ${
+                isDarkMode
+                  ? 'border-gray-700 text-gray-100'
+                  : 'border-gray-300 text-gray-900'
+              }`}
+            >
               Actions
             </th>
           </tr>
         </thead>
         <tbody>
           {editableData.map((target) => (
-            <tr key={target.id} className={`border-b ${isDarkMode ? 'border-gray-700 text-gray-100' : 'border-gray-300 text-gray-900'}`}>
-              <td className="py-2 px-4">
+            <tr
+              key={target.id}
+              className={`border-b ${
+                isDarkMode
+                  ? 'border-gray-700 text-gray-100'
+                  : 'border-gray-300 text-gray-900'
+              }`}
+            >
+              <td className='py-2 px-4'>
                 {editRowId === target.id ? (
                   <input
-                    type="text"
+                    type='text'
                     value={target.name}
                     onChange={(e) => handleInputChange(e, target.id, 'name')}
                     className={`w-full border p-1 rounded ${
-                      isDarkMode ? 'bg-gray-800 text-white border-gray-600' : 'bg-white text-black border-gray-300'
+                      isDarkMode
+                        ? 'bg-gray-800 text-white border-gray-600'
+                        : 'bg-white text-black border-gray-300'
                     }`}
                   />
                 ) : (
                   target.name
                 )}
               </td>
-              <td className="py-2 px-4">
+              <td className='py-2 px-4'>
                 {editRowId === target.id ? (
                   <input
-                    type="text"
+                    type='text'
                     value={target.description}
-                    onChange={(e) => handleInputChange(e, target.id, 'description')}
+                    onChange={(e) =>
+                      handleInputChange(e, target.id, 'description')
+                    }
                     className={`w-full border p-1 rounded ${
-                      isDarkMode ? 'bg-gray-800 text-white border-gray-600' : 'bg-white text-black border-gray-300'
+                      isDarkMode
+                        ? 'bg-gray-800 text-white border-gray-600'
+                        : 'bg-white text-black border-gray-300'
                     }`}
                   />
                 ) : (
                   target.description
                 )}
               </td>
-              <td className="py-2 px-4">
+              <td className='py-2 px-4'>
                 {editRowId === target.id ? (
                   <input
-                    type="text"
+                    type='text'
                     value={target.markets.join(', ')}
                     onChange={(e) => handleInputChange(e, target.id, 'markets')}
                     className={`w-full border p-1 rounded ${
-                      isDarkMode ? 'bg-gray-800 text-white border-gray-600' : 'bg-white text-black border-gray-300'
+                      isDarkMode
+                        ? 'bg-gray-800 text-white border-gray-600'
+                        : 'bg-white text-black border-gray-300'
                     }`}
                   />
                 ) : (
                   target.markets.join(', ')
                 )}
               </td>
-              <td className="py-2 px-4">
+              <td className='py-2 px-4'>
                 {new Date(target.lastUpdated).toLocaleDateString('en-GB')}
               </td>
-              <td className="py-2 px-4">
+              <td className='py-2 px-4'>
                 {editRowId === target.id ? (
                   <button
                     onClick={() => handleSave(target.id)}
-                    className="bg-blue-500 text-white px-4 py-1 rounded"
+                    className='bg-blue-500 text-white px-4 py-1 rounded'
                   >
                     Save
                   </button>
                 ) : (
                   <button
                     onClick={() => setEditRowId(target.id)}
-                    className="bg-yellow-500 text-white px-4 py-1 rounded"
+                    className='bg-yellow-500 text-white px-4 py-1 rounded'
                   >
                     Edit
                   </button>
